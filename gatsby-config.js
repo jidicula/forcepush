075d61f1d84316891ec2f11bebc4aebcc2c2b2f9
jidicula <johanan@forcepush.tech>
2021-05-29 12:52:59 -0400

    build(gatsby-config): Correct plugin entry for sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,21 @@ module.exports = {
     },
     "gatsby-plugin-offline",
     "gatsby-plugin-netlify",
-    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: true,
+        // deprecated options and their defaults:
+        base64Width: 20,
+        forceBase64Format: ``, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+      },
+    },
     "gatsby-remark-images",
     {
       resolve: "gatsby-source-filesystem",
