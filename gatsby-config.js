e106473a38b062e9fb737c994f2dc47da14c4ce1
jidicula <johanan@forcepush.tech>
2021-09-18 12:58:23 -0400

    test(gatsby-plugin-sharp): Force base64 format jpg

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,7 +76,7 @@ module.exports = {
         failOnError: true,
         // deprecated options and their defaults:
         base64Width: 20,
-        forceBase64Format: `png`, // valid formats: png,jpg,webp
+        forceBase64Format: `jpg`, // valid formats: png,jpg,webp
         useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
         stripMetadata: true,
         defaultQuality: 50,
